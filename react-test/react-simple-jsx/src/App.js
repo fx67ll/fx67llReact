86750aa9e59bc9fe652f8957f4e7fe43022f5e42
fx67ll <2717745014@qq.com>
2021-07-07 17:31:56 +0800

    add antd test

--- react-test/react-simple-jsx/src/App.js
+++ react-test/react-simple-jsx/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from 'antd';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+		  <Button type="primary">Ant Design Button</Button> <br/>
 		  react jsx test <br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
